05bdea3d0cf0a3482789f7854034cf8bdbce0b6b
jgushiann <njgus21@freeuni.edu.ge>
2025-12-08 14:31:53 +0400

    Initial commit

---
+++ src/main/java/org/service/allazaniuserservice/AllazaniUserServiceApplication.java
@@ -0,0 +1,13 @@
+package org.service.allazaniuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllazaniUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllazaniUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/service/allazaniuserservice/AllazaniUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.service.allazaniuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllazaniUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
