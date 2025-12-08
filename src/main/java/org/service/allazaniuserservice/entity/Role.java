a6341e356479056e76f15ada9b6f5e8f19a9bf92
jgushiann <njgus21@freeuni.edu.ge>
2025-12-08 17:09:58 +0400

    added entities

---
+++ src/main/java/org/service/allazaniuserservice/entity/Role.java
@@ -0,0 +1,6 @@
+package org.service.allazaniuserservice.entity;
+
+public enum Role {
+    ADMIN,
+    USER
+}
---
+++ src/main/java/org/service/allazaniuserservice/entity/User.java
@@ -0,0 +1,40 @@
+package org.service.allazaniuserservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "firstname")
+    private String firstname;
+
+    @Column(name = "lastname")
+    private String lastname;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
