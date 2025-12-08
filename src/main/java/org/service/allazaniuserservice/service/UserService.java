2d5d6a51ea31114212a5a6ec34744abe9a2aedf3
jgushiann <njgus21@freeuni.edu.ge>
2025-12-08 17:53:04 +0400

    created service class and updated user

---
+++ src/main/java/org/service/allazaniuserservice/service/UserService.java
@@ -0,0 +1,20 @@
+package org.service.allazaniuserservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.service.allazaniuserservice.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+    }
+}
